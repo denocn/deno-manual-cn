ae700c509458bdfd133413b4f7e7896e66aabd12
justjavac <justjavac@gmail.com>
2020-06-19 23:22:34 +0800

    迁移到 pagic

--- introduction.md
+++ introduction.md
@@ -1,6 +1,8 @@
 # 介绍
 
-{{#include __version.md}}
+> <a href="https://github.com/Nugine/deno-manual-cn/" rel="noopener noreferrer" target="_blank" style="display:inline-flex;align-items:center"><span style="margin-right:1em">deno-manual-cn</span><img alt="GitHub stars" src="https://img.shields.io/github/stars/Nugine/deno-manual-cn?style=social"> </a>
+> 
+> 此版本更新于 2020 年 6 月 8 日 [deno @ 62adc63](https://github.com/denoland/deno/tree/62adc63934d8be52b90512ad0e8165d9dd36eafc)
 
 Deno 是一个 JavaScript/TypeScript 的运行时，默认使用安全环境执行代码，有着卓越的开发体验。
 
---
+++ pagic.config.ts
@@ -0,0 +1,31 @@
+import { toc } from "./toc.ts";
+
+export default {
+  srcDir: ".",
+  ignore: [
+    /\/public\//,
+    /\/\.github\//,
+    /\/\.git\//,
+    /\/\.vscode\//,
+    /\/LICENSE/,
+    /\/pagic\.config\.ts/,
+    /\/toc\.ts/,
+  ],
+  theme: "docs",
+  plugins: ["sidebar", "script", "ga"],
+  title: "Deno 手册",
+  sidebar: toc,
+  nav: [
+    {
+      text: "GitHub",
+      link: "https://github.com/denocn/deno_manual",
+    },
+    {
+      text: "社区",
+      link: "http://deno.js.cn",
+    },
+  ],
+  ga: {
+    id: "UA-123999279-4",
+  },
+};
